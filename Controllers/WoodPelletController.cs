6b5816c0b964b7d0bde93eedf1ad45ddc6c6c780
Philip Preisler <phil313c@edu.zealand.dk>
2023-06-10 17:52:17 +0200

    Add project files.

---
+++ Controllers/WoodPelletController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using RestWoodPellets.Repositories;
+using WoodPelletsLib;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestWoodPellets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WoodPelletController : ControllerBase
+    {
+        private readonly WoodPelletsRepository _repository;
+        public WoodPelletController(WoodPelletsRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: api/<WoodPelletController>
+        [EnableCors("AllowAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public ActionResult<IEnumerable<WoodPellet>> Get()
+        {
+            List<WoodPellet> woodPellets = _repository.GetAll();
+            if (woodPellets == null) return NotFound("No Wood Pellet exist");
+            return Ok(woodPellets);
+        }
+
+        // GET api/<WoodPelletController>/5
+        [EnableCors("AllowAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<WoodPellet> Get(int id)
+        {
+            WoodPellet woodPellet = _repository.GetById(id);
+            if (woodPellet == null) return NotFound("No such id" + id);
+            return Ok(woodPellet);
+        }
+
+        // POST api/<WoodPelletController>
+        [EnableCors("AllowAll")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult<WoodPellet> Post([FromBody] WoodPellet value)
+        {
+            try
+            {
+                WoodPellet createdPellet = _repository.Add(value);
+                return Created($"api/WoodPelletController/{createdPellet.Id}", createdPellet);
+            }
+            catch (ArgumentException n)
+            {
+                return BadRequest(n.Message);
+            }
+        }
+        [EnableCors("AllowAll")]
+        // PUT api/<WoodPelletController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [HttpPut("{id}")]
+        public ActionResult<WoodPellet> Put(int id, [FromBody] WoodPellet value)
+        {
+            try
+            {
+                WoodPellet updatedWoodPellet = _repository.Update(id, value);
+                return Ok(updatedWoodPellet);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [EnableCors("AllowAll")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        // DELETE api/<WoodPelletController>/5
+        [HttpDelete("{id}")]
+        public ActionResult<WoodPellet> Delete(int id)
+        {
+            WoodPellet deletedPellet = _repository.Delete(id);
+            if (deletedPellet == null) return NotFound("No pellet deleted, id does not exist. Id:" + id);
+            return Ok(deletedPellet);
+        }
+    }
+}
+
---
+++ Repositories/WoodPelletsRepository.cs
@@ -0,0 +1,60 @@
+﻿using WoodPelletsLib;
+
+namespace RestWoodPellets.Repositories
+{
+    public class WoodPelletsRepository
+    {
+        private int _nextId = 1;
+        private readonly List<WoodPellet> Data;
+        public WoodPelletsRepository()
+        {
+            Data = new List<WoodPellet>
+            {
+                new WoodPellet{ Id = _nextId++, Brand = "BioWood", Price = 4995, Quality = 4 },
+                new WoodPellet{ Id = _nextId++, Brand = "BioWood", Price = 5195, Quality = 4 },
+                new WoodPellet{ Id = _nextId++, Brand = "BilligPille", Price = 4125, Quality = 1 },
+                new WoodPellet{ Id = _nextId++, Brand = "GoldenWoodPellet", Price = 5995, Quality = 5 },
+                new WoodPellet{ Id = _nextId++, Brand = "GoldenWoodPellet", Price = 5795, Quality = 5 }
+
+            };
+        }
+        public List<WoodPellet> GetAll()
+        {
+            if (Data.Count == 0)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                return new List<WoodPellet>(Data);
+            }
+        }
+        public WoodPellet? GetById(int id)
+        {
+            return Data.Find(WoodPellet => WoodPellet.Id == id);
+        }
+        public WoodPellet Add(WoodPellet newWoodPellet)
+        {
+            newWoodPellet.Validate(newWoodPellet);
+            newWoodPellet.Id = _nextId++;
+            Data.Add(newWoodPellet);
+            return newWoodPellet;
+        }
+        public WoodPellet Delete(int id)
+        {
+            WoodPellet? woodPellet = GetById(id);
+            if (woodPellet == null) return null;
+            Data.Remove(woodPellet);
+            return woodPellet;
+        }
+        public WoodPellet? Update(int id, WoodPellet updates)
+        {
+            WoodPellet? woodPellet = GetById(id);
+            if (woodPellet == null) return null;
+            woodPellet.Brand = updates.Brand;
+            woodPellet.Price = updates.Price;
+            woodPellet.Quality = updates.Quality;
+            return woodPellet;
+        }
+    }
+}
