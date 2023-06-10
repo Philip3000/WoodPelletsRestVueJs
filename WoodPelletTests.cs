963ff581cbad823c813a5c2b041ba09c7cd2edf8
Philip Preisler <78653614+Philip3000@users.noreply.github.com>
2023-06-10 18:00:44 +0200

    Add files via upload

---
+++ WoodPelletTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WoodPelletsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoodPelletsLib.Tests
+{
+    [TestClass()]
+    public class WoodPelletTests
+    {
+        WoodPellet woodPellet = new WoodPellet();
+       
+        [TestMethod()]
+        public void ValidateBrandTest()
+        {
+            woodPellet.Brand = "h";
+            Assert.ThrowsException<ArgumentException>(() => woodPellet.ValidateBrand(woodPellet.Brand));
+        }
+
+
+        [TestMethod()]
+        public void ValidateQualityTest()
+        {
+            woodPellet.Quality = 6;
+            Assert.ThrowsException<ArgumentException>(() => woodPellet.ValidateQuality(woodPellet.Quality));
+            woodPellet.Quality = 0;
+            Assert.ThrowsException<ArgumentException>(() => woodPellet.ValidateQuality(woodPellet.Quality));
+        }
+    }
+}
